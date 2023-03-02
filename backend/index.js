3c970738c4ef3ecd0192a4d824ae2c77d795c700
julandrod <julian.andres.rodriguez@gmail.com>
2023-03-02 14:04:07 -0500

    (backend) <add> api index and configuration files

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,52 @@
-import express from "express";
-import dotenv from "dotenv";
-import startDb from "./db/index.js";
+require("dotenv").config();
+const express = require("express");
 
+// import database
+const db = require("./models");
+
+// import routes
+const userRoutes = require("./routes/user.routes");
+const authRoutes = require("./routes/auth.routes");
+const roleRoutes = require("./routes/roles.routes");
+const accountsRoutes = require("./routes/accounts.routes");
+const transactionsRoutes = require("./routes/transactions.routes");
+
+// Middlewares
+const errorHandler = require("./middlewares/error.middleware");
+const { ErrorObject } = require("./helpers");
+
+// API config
 const app = express();
-dotenv.config();
 const port = process.env.PORT || 8080;
+app.use(express.json());
 
-// Routes
+// API routes
 app.get("/", (req, res) => {
   res.send("Virtual Wallet");
 });
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/roles", roleRoutes);
+app.use("/api/v1/accounts", accountsRoutes);
+app.use("/api/v1/transactions", transactionsRoutes);
 
-const startApi = () => {
-  startDb();
-  app.listen(port, () => console.log(`Server is listening on port ${port}`));
+// Handle errors
+app.use((req, res, next) => {
+  next(new ErrorObject("No se encontro la ruta", 404));
+});
+app.use(errorHandler);
+
+const startApi = async () => {
+  console.log("Testing the database connection ...");
+  try {
+    await db.sequelize.authenticate();
+    console.log("Database authentication successfully");
+    await db.sequelize.sync({ alter: true, logging: false });
+    console.log("Database synchronized");
+    app.listen(port, () => console.log(`Server start on port ${port}`));
+  } catch (error) {
+    console.log("Unable to connect to the database ", error);
+  }
 };
 
 startApi();
