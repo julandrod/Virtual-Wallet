f15ce7e5fb6e8eb1c36639773b9a1638ed4a115c
julandrod <julian.andres.rodriguez@gmail.com>
2023-03-02 11:08:40 -0500

    (backend) <add> error middleware to handle errors throw in the api

--- backend/middlewares/error.middleware.js
+++ backend/middlewares/error.middleware.js
@@ -1,7 +1,15 @@
-const errorHandlerMiddleware = (err, req, res, next) => {
-  const status = err.statusCode || 500;
-  const message = err.message || err;
-  res.status(status).json({ message });
+const errorHandler = (err, req, res, next) => {
+  let code = err.statusCode || 500;
+  let message = err.message || err;
+
+  // if (err.errors.length > 0) {
+  //   message = Object.values(err.errors)
+  //     .map((item) => item.message)
+  //     .join(", ");
+  //   code = 400;
+  // }
+
+  res.status(code).json({ message });
 };
 
-export default errorHandlerMiddleware;
+module.exports = errorHandler;
